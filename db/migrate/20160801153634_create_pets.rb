a573a9f3a4300ff02a5956a732724cc2ebbe5114
mrqrvly <mrqrvly@gmail.com>
2016-08-01 10:58:02 -0500

    migrated pets schema

---
+++ db/migrate/20160801153634_create_pets.rb
@@ -0,0 +1,15 @@
+class CreatePets < ActiveRecord::Migration
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :type
+      t.string :breed
+      t.string :address
+      t.string :organization
+      t.string :phone
+      t.string :website
+      t.string :description
+      t.string :userid
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801035407) do
+ActiveRecord::Schema.define(version: 20160801153634) do
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.string "breed"
+    t.string "address"
+    t.string "organization"
+    t.string "phone"
+    t.string "website"
+    t.string "description"
+    t.string "userid"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
