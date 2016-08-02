76ff2b3583c1b767c086c1b0bb3955795c93dd7f
mrqrvly <mrqrvly@gmail.com>
2016-08-02 09:17:05 -0500

    tuesday morning merge

--- app/public/js/petapi.js
+++ app/public/js/petapi.js
@@ -1,24 +1,22 @@
 console.log('Sound the trumpets, JavaScript is here!!!');
 
-
+var breeds = []
 
 $.ajax({
   url:'http://api.petfinder.com/breed.list',
   type: 'GET',
   data: {
-    key: "61635e39395ce71e4d0eba82c79adb55",
+    key:    "61635e39395ce71e4d0eba82c79adb55",
     format: "json",
     animal: "dog",  
     output: "full"
   },
   dataType: 'jsonp',
-  success: function(result){
+  success: function(result) {
     console.log(result);
-    (result.petfinder.breeds.breed).forEach(function(breed) {
-      console.log(breed[0]);
-    })
-    console.log(result.petfinder.breeds.breed[0]);
+    for (var index in result.petfinder.breeds.breed) {
+      breeds.push(result.petfinder.breeds.breed[index].$t);
+    }
+    console.log(JSON.stringify(breeds.join()));
   }
 });
-
-
