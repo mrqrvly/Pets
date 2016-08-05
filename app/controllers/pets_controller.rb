ccf681ccf8395f7555f59b82133f97db7ba28a07
mrqrvly <mrqrvly@gmail.com>
2016-08-05 00:16:25 -0500

    just trying to finish basic stuff at this point

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,18 +6,21 @@ require 'json'
 
 class PetsController < ApplicationController
 
-  #  GET - /pets/petprofile/ - Allows access to a single result of Ajax request
+  
+
+  #  POST - /pets/petprofile/ - Allows access to a single result of Ajax request
   #  -------------------------------------------------------------------------
-  get '/petprofile/?' do
+  post '/petsave/?' do
+    # pet = Pet.create name: 
+    @pet = params[:petdata]
+    puts params
     
+    erb :pet
+
+
   end
 
-  #  GET - /pets/ - Unused as of right now
-  #  -------------------------------------
-  get '/?' do
-    # erb :results
-  end
-
+  
 
   #  POST - /pets/ - Renders Ajax search results and map
   #                  sets params for us in Ajax request
@@ -28,32 +31,42 @@ class PetsController < ApplicationController
     @breed  = params[:breed]
     @zip    = params[:zip]
     
-    result  = HTTParty.get("http://api.petfinder.com/pet.find?key=61635e39395ce71e4d0eba82c79adb55&location=#{@zip}&animal=#{@animal}&breed=#{@breed}&count=9&format=json").parsed_response
+    result  = HTTParty.get("http://api.petfinder.com/pet.find?key=61635e39395ce71e4d0eba82c79adb55&location=#{@zip}&animal=#{@animal}&breed=#{@breed}&count=21&format=json").parsed_response
     
-    petArray = []
+    @petArray = []
 
-    for pet in result["petfinder"]["pets"]["pet"] do
-      puts "=============================================================="
-
-      pet = {"name"=>pet["name"]["$t"],
-             "animal"=>pet["animal"]["$t"],
-             "breed"=>@breed,
-             "description"=>pet["description"]["$t"],
-             "phone"=>pet["contact"]["phone"]["$t"],
-             "email"=>pet["contact"]["email"]["$t"],
-             "address"=>pet["contact"]["address1"]["$t"],
-             "city"=>pet["contact"]["city"]["$t"],
-             "state"=>pet["contact"]["state"]["$t"],
-             "zip"=>pet["contact"]["zip"]["$t"]
+    for animal in result["petfinder"]["pets"]["pet"] do
+      pet = {"name"        =>animal["name"]["$t"],
+             "animal"      =>animal["animal"]["$t"],
+             "breed"       =>@breed,
+             "description" =>animal["description"]["$t"],
+             "phone"       =>animal["contact"]["phone"]["$t"],
+             "email"       =>animal["contact"]["email"]["$t"],
+             "address"     =>animal["contact"]["address1"]["$t"],
+             "city"        =>animal["contact"]["city"]["$t"],
+             "state"       =>animal["contact"]["state"]["$t"],
+             "zip"         =>animal["contact"]["zip"]["$t"],
+             "picsmall"    =>animal["media"]["photos"]["photo"][1]["$t"],
+             "petid"       =>animal["id"]["$t"]
+             # "piclarge"    =>animal["media"]["photos"]["photo"][3]["$t"]
             }
 
-      petArray.push(pet)
+    # if animal["media"]["photos"]["photo"][1]["$t"]
+    #   pet[:picsmall] = animal["media"]["photos"]["photo"][1]["$t"]
+    # else
+    #   pet[:picsmall] = "No Photo"
+    # end
 
-      puts "=============================================================="
+    # if animal["media"]["photos"]["photo"][2]["$t"]
+    #   pet[:piclarge] = animal["media"]["photos"]["photo"][2]["$t"]
+    # else
+    #   pet[:piclarge] = "No Photo"
+    # end
+      @petArray.push(pet)
+    
+      puts "================"
+      puts pet
     end
-
-    puts petArray[0]["name"]
-
 
     erb :results
   end
